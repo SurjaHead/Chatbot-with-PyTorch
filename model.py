f6840c2e12fa752d612e0a6df995e54c8ea15247
SurjaHead <95391476+SurjaHead@users.noreply.github.com>
2022-12-15 19:52:23 -0500

    Update model.py

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 
-
+# creating the neural network that we'll use to train the data
 class NeuralNet(nn.Module):
     def __init__(self, input_size, hidden_size, num_classes):
         super(NeuralNet, self).__init__()
